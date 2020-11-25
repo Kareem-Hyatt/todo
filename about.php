34634665d029532631771472579f2e693b205e94
Kareem-Hyatt <46932153+Kareem-Hyatt@users.noreply.github.com>
2020-11-25 15:54:13 -0500

    Fist Push
    Final Assignment. Lets do this.

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+    $title = 'About';
+    require_once 'includes/header.php';
+?>
+
+<h3 align="justify" >At ToDo by Kareem Hyatt, our mission is to make life easy and simple so that people can concentrate on what really matters in their life. 
+    Todo by Kareem Hyatt not only makes people effortlessly stay organized but also makes sure that they never miss important things. 
+    Todo by Kareem Hyatt aims to simplify taking notes and make them availabe at the touch of a button. 
+    This is accomplished by providing data storage in the cloud that can be accessed worldwide.</h3>
+    <br>
+<p align="justify">Todo by Kareem Hyatt was founded in 2020 as a part of a final assignment for an Applied Web Developement course at VTDI.</p>
+
+<?php require_once 'includes/footer.php'; ?>
+
+
+
+
